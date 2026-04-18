33b97850711af9f03b2c52c424172ff5945ddb89
kbsha <kibrunpa1@gmail.com>
2026-04-18 17:17:25 +0200

    chnage the  Jinja2Templates to static and improve the static/index.html for better front end

--- agent.py
+++ agent.py
@@ -80,4 +80,5 @@ def run_agent(user_input: str) -> str:
         return result["messages"][-1].content
     except Exception as e:
         return f"Error: {str(e)}"
-print(run_agent("Write a note to 'meeting_notes.txt' with the content 'Meeting at 3 PM on Friday.'"))
+# print(run_agent("Write a note to 'meeting_notes.txt' with the content 'Meeting at 3 PM on Friday.'"))
+# print(run_agent("hello, how are you?"))
